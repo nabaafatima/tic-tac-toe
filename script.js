c570f649db7d20d61ad6388fbb16c56329f223cc
nabaafatima <nabaafatima7097@gmail.com>
2026-06-15 17:45:02 +0500

    made the wining logic work

--- script.js
+++ script.js
@@ -2,23 +2,41 @@ const resetgame = document.getElementById("reset");
 const playerturn = document.getElementById("turn");
 const cells = document.querySelectorAll('.cell');
 const color = document.getElementById("main");
-isplaying = true;
-let winmimgpattern = [[1,2,3] , [4, 5,6], [7,8,9] , [1,5,9],[3,5,7], [1,4,7] , [2,4,8],[3,6,9] ];
+let isplaying = true;
+let winningpattern = [[0,1,2], [3, 4, 5], [6, 7, 8], [1, 4, 7],[0,3,6] ,[2, 5, 8],[2, 4, 6]];
 let player1turn = true;
-cells.forEach((cell) =>{
-    
+let boardState = ["", "", "", "", "", "", "", "", ""];
+cells.forEach((cell) =>{    
     cell.addEventListener("click", (e)=>{   
-        if (e.target.innerText !== "") return;
+        if (e.target.innerText !== ""|| !isplaying) return;
+        const cellIndex = parseInt(e.target.getAttribute("data-index"));
+        let currentsymbol = "";
         if(player1turn===true) {                                
             color.style.backgroundColor = "red";
             e.target.innerText = "X";
-            playerturn.innerText = "player 2";
+            playerturn.innerText = "player 2(O)";
+            currentsymbol = "X";
             player1turn = false;
         } else if(player1turn===false) {
             color.style.backgroundColor = "blue";
             e.target.innerText = "O";
             playerturn.innerText = "player 1";
+            currentsymbol = "O";
             player1turn = true;
         };
+        boardState[cellIndex] = currentsymbol;
+        checkwinner(currentsymbol); 
     })
 });
+function checkwinner(symbol) {
+    const cellarray = Array.from(cells);
+    const gamewon = winningpattern.some(pattern =>{
+        return pattern.every(index=>{
+            return boardState[index] === symbol;
+        });
+    });
+    if (gamewon) {
+        playerturn.innerText = `${symbol} Wins the Game!`;
+        isplaying = false;
+    }
+}
