a52afdfd0fc4bc2483b8b1f420f476117f76a2ae
nabaafatima <nabaafatima7097@gmail.com>
2026-06-15 17:15:31 +0500

    finally made the logic for x and O work

--- script.js
+++ script.js
@@ -5,21 +5,20 @@ const color = document.getElementById("main");
 isplaying = true;
 let winmimgpattern = [[1,2,3] , [4, 5,6], [7,8,9] , [1,5,9],[3,5,7], [1,4,7] , [2,4,8],[3,6,9] ];
 let player1turn = true;
-let player2turn = false;
-cells.forEach(addEventListener("click", game()));
-function game() {
-     if (player1turn == true) {
-        this.innerText = "X";
-        player1turn = false;
-        player2turn = true;
-        playerturn.innerText = "Player 2"
-        color.style.backgroundColor = "red";
-
-    } else {
-        this.innerText = "O";
-        player1turn = true;
-        player2turn = false;
-        playerturn.innerText = "Player 1"
-        color.style.backgroundColor = "blue";
-    };
-};
+cells.forEach((cell) =>{
+    
+    cell.addEventListener("click", (e)=>{   
+        if (e.target.innerText !== "") return;
+        if(player1turn===true) {                                
+            color.style.backgroundColor = "red";
+            e.target.innerText = "X";
+            playerturn.innerText = "player 2";
+            player1turn = false;
+        } else if(player1turn===false) {
+            color.style.backgroundColor = "blue";
+            e.target.innerText = "O";
+            playerturn.innerText = "player 1";
+            player1turn = true;
+        };
+    })
+});
