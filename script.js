fb5ef42b04ed701b5c87729c5b391ab85e1d68ba
nabaafatima <nabaafatima7097@gmail.com>
2026-06-15 18:06:59 +0500

    made the reset and the pop up work

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+const winModal = document.getElementById("winModal");
+const winMessage = document.getElementById("winMessage");
+const closeModal = document.getElementById("closeModal");
 const resetgame = document.getElementById("reset");
 const playerturn = document.getElementById("turn");
 const cells = document.querySelectorAll('.cell');
 const color = document.getElementById("main");
 let isplaying = true;
-let winningpattern = [[0,1,2], [3, 4, 5], [6, 7, 8], [1, 4, 7],[0,3,6] ,[2, 5, 8],[2, 4, 6]];
+let winningpattern = [[0,1,2], [3, 4, 5], [6, 7, 8], [1, 4, 7],[0,3,6] ,[2, 5, 8],[0, 4, 8],[2, 4, 6]];
 let player1turn = true;
 let boardState = ["", "", "", "", "", "", "", "", ""];
+function resetTheGame() {
+    boardState = ["", "", "", "", "", "", "", "", ""];
+    cells.forEach(cell => {
+        cell.innerText = "";
+    });
+    isplaying = true;
+    player1turn = true;
+    playerturn.innerText = "Player 1(X)";
+    color.style.backgroundColor = "#ff69b4";
+}
+
 cells.forEach((cell) =>{    
     cell.addEventListener("click", (e)=>{   
         if (e.target.innerText !== ""|| !isplaying) return;
@@ -20,7 +34,7 @@ cells.forEach((cell) =>{
         } else if(player1turn===false) {
             color.style.backgroundColor = "blue";
             e.target.innerText = "O";
-            playerturn.innerText = "player 1";
+            playerturn.innerText = "player 1(X)";
             currentsymbol = "O";
             player1turn = true;
         };
@@ -36,7 +50,14 @@ function checkwinner(symbol) {
         });
     });
     if (gamewon) {
-        playerturn.innerText = `${symbol} Wins the Game!`;
+        let winnerName = symbol === "X" ? "Player 1 (X)" : "Player 2 (O)";
+        winMessage.innerHTML = `Woohoo! <br>${winnerName} totally crushed it!`;
+        winModal.style.display = "flex";
         isplaying = false;
     }
 }
+resetgame.addEventListener("click", resetTheGame);
+closeModal.addEventListener("click", () => {
+    winModal.style.display = "none";
+    resetTheGame();
+});
